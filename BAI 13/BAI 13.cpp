89b3edda695bd3b3eccacbf62ebf40082c176b9b
hoanga4dc <115219822+hoanga4dc@users.noreply.github.com>
2022-10-20 10:34:00 +0700

    Delete BAI 13.cpp

--- BAI 13/BAI 13.cpp
+++
@@ -1,9 +0,0 @@
-#include"Manager_Employee.cpp"
-#include"Menu.cpp"
-int Employee::Employee_count = 0;
-int main() {
-	shared_ptr<Manager_Employee> Q(new Manager_Employee);
-	Menu X(Q);
-	X.Run();
-	return 0;
-}
